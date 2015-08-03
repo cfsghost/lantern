ae35d0a41f3ff7bcf0ed158f1ca57a72da47e4d1
Fred Chien <cfsghost@gmail.com>
2015-08-03 20:17:08 +0800

    completed sign in page

--- src/js/components/SignInPage.jsx
+++ src/js/components/SignInPage.jsx
@@ -1,18 +1,53 @@
 var React = require('react');
-var Link = require('react-router').Link;
+var Link = require('react-router-component').Link;
 
 var Header = require('./Header.jsx');
 
 class SignInPage extends React.Component {
+
+	componentWillMount() {
+	}
+
 	render() {
 		return (
 			<div>
 				<Header />
-				<div className={'ui basic center aligned segment'}>
-					<div className={'ui left icon input'}>
-						<i className={'user icon'} />
-						<input type='text' name='email' placeholder='E-mail address' />
+				<div className='ui hidden divider'></div>
+				<div className='ui hidden divider'></div>
+				<div className={'ui basic center aligned padded segment'}>
+
+					<div className='ui two column centered grid'>
+						<div className='column'>
+							<h1 className='ui header'>
+								<i className='sign in icon' />
+								<div className='content'>Sign In</div>
+							</h1>
+							<div className={'ui basic segment'}>
+								<div className='ui form'>
+									<div className='field'>
+										<div className={'ui left icon input'}>
+											<i className={'user icon'} />
+											<input type='text' name='email' placeholder='E-mail address' autoFocus={true} />
+										</div>
+									</div>
+									<div className='field'>
+										<div className={'ui left icon input'}>
+											<i className={'lock icon'} />
+											<input type='password' name='password' placeholder='Password' />
+										</div>
+									</div>
+									<div className='field'>
+										<button className='ui teal button'>Sign In</button>
+									</div>
+									<div className='field ui teal message'>
+										No Account yet? <Link href='/signup'>Sign Up</Link>
+									</div>
+								</div>
+							</div>
+						</div>
+
 					</div>
+
 				</div>
 			</div>
 		);
