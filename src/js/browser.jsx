d0193d8a3455ebc4a68b9bdfbd8e4b631df508a0
Fred Chien <cfsghost@gmail.com>
2015-08-20 20:05:01 +0800

    update fluky

--- src/js/browser.jsx
+++ src/js/browser.jsx
@@ -2,10 +2,6 @@ var React = require('react');
 var Fluky = require('fluky');
 var App = require('./app.jsx');
 
-if (window.Fluky) {
-	Fluky.setInitialState(window.Fluky.state);
-}
-
 require('../less/theme.less');
 
 // Rendering immediately
