5600fcbfce8eb189a8134e4728e84e1a6695d77f
Fred Chien <cfsghost@gmail.com>
2015-08-22 22:22:14 +0800

    update NotFoundPage

--- src/js/components/NotFoundPage.jsx
+++ src/js/components/NotFoundPage.jsx
@@ -4,7 +4,9 @@ class NotFoundPage extends React.Component {
 
 	render() {
 		return (
-			<div>404 Not Found</div>
+			<div className='ui basic segment'>
+				<h1 className='ui center aligned huge header'>Oops, Not Found</h1>
+			</div>
 		);
 	}
 }
