2821b9805682b34baaabc77b13c099061fade3b7
Fred Chien <cfsghost@gmail.com>
2015-09-07 05:44:25 +0800

    rename store

--- src/js/stores/index.js
+++ src/js/stores/index.js
@@ -1,11 +1,11 @@
 import user from './user';
 import admin from './admin';
-import admin_user from './admin_user';
+import admin_users from './admin_users';
 import admin_dashboard from './admin_dashboard';
 
 export default {
 	user: user,
 	admin: admin,
-	admin_user: admin_user,
+	admin_users: admin_users,
 	admin_dashboard: admin_dashboard
 };
