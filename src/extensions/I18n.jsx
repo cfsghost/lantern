b3aac0458dc87d70ddfd9f42e12e5d17e9c58456
Fred Chien <cfsghost@gmail.com>
2015-10-06 09:51:27 +0800

    Fixed a bug that I18n is not working correctly if string contains react components

--- src/extensions/I18n.jsx
+++ src/extensions/I18n.jsx
@@ -17,10 +17,28 @@ class I18n extends React.Component {
 
 	render() {
 		var msg = '';
-		if (!this.props.args) {
-			msg = Fluky.locale.getMessage(this.props.sign, this.props.children); 
+
+		// Convert to pure HTML if it contains react components
+		var children = [];
+		if (Array.isArray(this.props.children)) {
+			children = [];
+
+			for (var index in this.props.children) {
+				var child = this.props.children[index];
+
+				if (typeof child === 'string')
+					children.push(child);
+				else
+					children.push(React.renderToString(child));
+			}
 		} else {
-			var args = [ this.props.sign, this.props.children ].concat(this.props.args);
+			children.push(this.props.children);
+		}
+
+		if (!this.props.args) {
+			msg = Fluky.locale.getMessage(this.props.sign, children.join('')); 
+		} else {
+			var args = [ this.props.sign, children.join('') ].concat(this.props.args);
 			msg = Fluky.locale.getFmtMessage.apply(this, args); 
 		}
 
