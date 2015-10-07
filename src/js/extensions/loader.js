3dcd91ba6ea6a0922b041ad22c8b94e243ddb43d
Fred Chien <cfsghost@gmail.com>
2015-10-08 02:58:42 +0800

    do not load script and css twice by using loader extension

--- src/js/extensions/loader.js
+++ src/js/extensions/loader.js
@@ -4,8 +4,18 @@ export default function *() {
 
 	var ctx = this;
 
+	var store = this.getState('Loader', {
+		css: [],
+		script: []
+	});
+
 	this.loader = {
 		css: (url) => {
+			// It was already loaded
+			if (store.css.indexOf(url) != -1)
+				return;
+
+			store.css.push(url);
 			$('<link rel="stylesheet" type="text/css" href="' + url + '">').appendTo('head');
 		},
 		script: (url, cb) => {
@@ -23,6 +33,11 @@ export default function *() {
 
 					for (var index in urls) {
 
+						// It was already loaded
+						if (store.script.indexOf(urls[index]) != -1)
+							continue;
+
+						store.script.push(urls[index]);
 						yield function(done) {
 							scriptjs(urls[index], done);
 						};
