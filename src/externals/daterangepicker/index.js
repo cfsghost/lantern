2e80165b9e99a936ee777c089ec83299ef0923d5
Fred Chien <cfsghost@gmail.com>
2015-10-08 01:49:05 +0800

    loading daterangepicker if it is on client-side

--- src/externals/daterangepicker/index.js
+++ src/externals/daterangepicker/index.js
@@ -1,4 +1,8 @@
-import daterangepicker from './daterangepicker';
+
+import Fluky from 'fluky';
+
+if (Fluky.isBrowser)
+	import daterangepicker from './daterangepicker';
 
 export default {
 	name: 'DateRangePicker'
