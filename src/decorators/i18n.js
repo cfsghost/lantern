d5996420a1ed6637ea06e6c529babb5badd0329d
Fred Chien <cfsghost@gmail.com>
2015-11-01 08:40:16 +0800

    added flux dependency to i18n decorator

--- src/decorators/i18n.js
+++ src/decorators/i18n.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import flux from './flux';
 
 export default function(target) {
+	flux(target);
 
 	target.prototype.__defineGetter__('i18n', function() {
 		return this.context.flux.locale;
