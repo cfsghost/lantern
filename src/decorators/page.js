f1a0b06fca20dd91a350d911be1005d9fc299360
Fred Chien <cfsghost@gmail.com>
2015-11-07 03:32:47 +0800

    add page static to store page settings

--- src/decorators/page.js
+++ src/decorators/page.js
@@ -4,6 +4,12 @@ export default function(page) {
 
 	return function(Component) {
 
+		if (!Component.isInitializer) {
+			Component.page = page;
+		} else {
+			Component.component.page = page;
+		}
+
 		var pageInit = (handle) => {
 			if (page.title)
 				handle.doAction('Window.setTitle', page.title);
