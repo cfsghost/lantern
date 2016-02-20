0a6c44aee353584091a409c6a5420a31190dece9
Fred Chien <cfsghost@gmail.com>
2016-02-21 07:11:10 +0800

    divided LoginState from Header

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -6,7 +6,7 @@ import I18n from 'Extension/I18n.jsx';
 import { router, flux, i18n, preAction } from 'Decorator';
 
 // Components
-import Avatar from './Avatar.jsx';
+import LoginState from './LoginState.jsx';
 
 @flux
 @router
@@ -31,14 +31,6 @@ class Header extends React.Component {
 		this.flux.off('state.Service', this.onChange);
 	};
 
-	componentDidMount() {
-
-		// Enabling dropdown menu
-		$(this.refs.component).find('.ui.dropdown').dropdown({
-			on: 'hover'
-		});
-	}
-
 	onChange = () => {
 
 		this.setState({
@@ -48,60 +40,13 @@ class Header extends React.Component {
 	};
 
 	render() {
-
-		var loginState;
-		if (this.state.user.logined) {
-			var adminItem;
-			if (this.state.user.permissions.admin) {
-				if (this.state.user.permissions.admin.access) {
-					adminItem = (
-						<Link to='/admin' className='item'>
-							<i className='spy icon'></i>
-							<I18n sign='header.menu.admin_panel'>Admin Panel</I18n>
-						</Link>
-					);
-				}
-			}
-
-			loginState = (
-				<div className={'right menu'}>
-					<div className='ui dropdown item'>
-						<span><Avatar hash={this.state.user.avatar_hash} size={20} /> <span>{this.state.user.name}</span></span>
-						<i className='dropdown icon'></i>
-						<div className='menu'>
-							<Link to='/settings' className='item'>
-								<i className='settings icon'></i>
-								<I18n sign='header.menu.settings'>Settings</I18n>
-							</Link>
-							{adminItem}
-							<div className='ui fitted divider'></div>
-							<a href='/signout' className='item'>
-								<i className='sign out icon'></i>
-								<I18n sign='header.menu.sign_out'>Sign Out</I18n>
-							</a>
-						</div>
-					</div>
-				</div>
-			);
-		} else {
-			loginState = (
-				<div className={'right menu'}>
-					<Link to='/signin'>
-						<div className={'item'}>
-							<i className={'sign in icon'} />
-							<I18n sign='header.menu.sign_in'>Sign In</I18n>
-						</div>
-					</Link>
-				</div>
-			);
-		}
-
 		return (
 			<div ref='component' className={'ui top fixed inverted menu'}>
 				<Link to='/' className={'item'} activeClassName=''>
 					<div>{this.state.service.name}</div>
 				</Link>
-				{loginState}
+
+				<LoginState user={this.state.user} />
 			</div>
 		);
 	}
---
+++ src/js/components/LoginState.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Link } from 'react-router';
+import I18n from 'Extension/I18n.jsx';
+
+// Components
+import Avatar from './Avatar.jsx';
+
+class LoginState extends React.Component {
+
+	componentDidMount() {
+
+		// Enabling dropdown menu
+		if (this.refs.dropdownMenu) {
+			$(this.refs.dropdownMenu).dropdown({
+				on: 'hover'
+			});
+		}
+	}
+
+	render() {
+
+		var loginState;
+		if (this.props.user.logined) {
+			var adminItem;
+			if (this.props.user.permissions.admin) {
+				if (this.props.user.permissions.admin.access) {
+					adminItem = (
+						<Link to='/admin' className='item'>
+							<i className='spy icon'></i>
+							<I18n sign='header.menu.admin_panel'>Admin Panel</I18n>
+						</Link>
+					);
+				}
+			}
+
+			loginState = (
+				<div className={'right menu'}>
+					<div ref='dropdownMenu' className='ui dropdown item'>
+						<span><Avatar hash={this.props.user.avatar_hash} size={20} /> <span>{this.props.user.name}</span></span>
+						<i className='dropdown icon'></i>
+						<div className='menu'>
+							<Link to='/settings' className='item'>
+								<i className='settings icon'></i>
+								<I18n sign='header.menu.settings'>Settings</I18n>
+							</Link>
+							{adminItem}
+							<div className='ui fitted divider'></div>
+							<a href='/signout' className='item'>
+								<i className='sign out icon'></i>
+								<I18n sign='header.menu.sign_out'>Sign Out</I18n>
+							</a>
+						</div>
+					</div>
+				</div>
+			);
+		} else {
+			loginState = (
+				<div className={'right menu'}>
+					<Link to='/signin'>
+						<div className={'item'}>
+							<i className={'sign in icon'} />
+							<I18n sign='header.menu.sign_in'>Sign In</I18n>
+						</div>
+					</Link>
+				</div>
+			);
+		}
+
+		return loginState;
+	}
+}
+
+export default LoginState;
