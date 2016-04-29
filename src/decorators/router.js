a32d702bc91456a65b3e9c8d9606d14b2f4e873d
Fred Chien <cfsghost@gmail.com>
2016-04-29 15:02:53 +0800

    support newest version of react-router

--- src/decorators/router.js
+++ src/decorators/router.js
@@ -10,8 +10,13 @@ export default function(target) {
 		Component.contextTypes = {};
 
 	Component.contextTypes.history = React.PropTypes.object;
+	Component.contextTypes.router = React.PropTypes.object;
 
 	Component.prototype.__defineGetter__('history', function() {
 		return this.context.history;
+	});
+
+	Component.prototype.__defineGetter__('router', function() {
+		return this.context.router;
 	});
 };
