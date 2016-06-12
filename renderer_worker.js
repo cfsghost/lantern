d662ccd5caee6858904c8056fec9b9704a44231e
Fred Chien <cfsghost@gmail.com>
2016-06-12 16:55:33 +0800

    improve performance of rendering worker

--- renderer_worker.js
+++ renderer_worker.js
@@ -14,43 +14,43 @@ ReactApp.init({
 	externalUrl: Utils.getExternalUrl()
 });
 
-process.on('message', function(task) {
+var pageHandler = co.wrap(function *(task) {
 
-	co(function *() {
-
-		// Rendering page with current state and cookie to client-side
-		var page = yield ReactApp.render(task.data.path, task.data.curState, {
-			cookie: task.data.cookie
-		});
-
-		var result = [ task.id ];
-
-		// Redirect
-		if (page.redirect) {
-			result.push('D');
-			result.push(page.redirect);
-			process.send(result.join(''));
-			return;
-		}
-
-		// Using service name by default
-		if (!page.state.Window.title) {
-			page.state.Window.title = settings.general.service.name;
-		}
-
-		// Rendering page
-		consolidate.jade('views/index.jade', {
-			cache: true,
-			title: page.state.Window.title,
-			content: page.content,
-			window: page.state.Window,
-			state: JSON.stringify(page.state)
-		}, function(err, html) {
-
-			result.push('C');
-			result.push(html);
-			process.send(result.join(''));
-		});
-
+	// Rendering page with current state and cookie to client-side
+	var page = yield ReactApp.render(task.data.path, task.data.curState, {
+		cookie: task.data.cookie
 	});
+
+	var result = [ task.id ];
+
+	// Redirect
+	if (page.redirect) {
+		result.push('D');
+		result.push(page.redirect);
+		process.send(result.join(''));
+		return;
+	}
+
+	// Using service name by default
+	if (!page.state.Window.title) {
+		page.state.Window.title = settings.general.service.name;
+	}
+
+	// Rendering page
+	consolidate.jade('views/index.jade', {
+		cache: true,
+		title: page.state.Window.title,
+		content: page.content,
+		window: page.state.Window,
+		state: JSON.stringify(page.state)
+	}, function(err, html) {
+
+		result.push('C');
+		result.push(html);
+		process.send(result.join(''));
+	});
+});
+
+process.on('message', function(task) {
+	pageHandler(task);
 });
