9345b076093d2b69c16eb35a54b3f7dd9939c7d7
Fred Chien <cfsghost@gmail.com>
2016-06-17 00:58:32 +0800

    remove dist before rebuild

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,7 +1,11 @@
 process.env.NODE_ENV = 'production';
 
+var path = require('path');
+var fse = require('fs-extra');
 var webpack = require('webpack');
 var configs = require('./webpack.config.js');
+
+fse.removeSync(path.join(__dirname, 'dist'));
 
 configs.forEach(function(config) {
 	if (!config.plugins)
