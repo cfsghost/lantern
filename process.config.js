2df7499e5aabde0d8370596fe1f3fddc7ce0c19a
leonlin <leonlin14@gmail.com>
2017-05-20 20:44:56 +0800

    update packages version and add pm2 settings file

---
+++ process.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var fs = require('fs');
+var settings = {};
+
+try {
+	var data = fs.readFileSync(path.join(__dirname, 'configs', 'general.json'));
+	settings.general = JSON.parse(data);
+} catch(e) {
+	var data = fs.readFileSync(path.join(__dirname, 'configs', 'general.json.default'));
+	settings.general = JSON.parse(data);
+}
+
+module.exports = {
+	apps : [{
+	    "script": "./app.js",
+	    "name": settings.general.service.name,
+	    "exec_interpreter": "node",
+	    "interpreter_args": "--harmony",
+	    "exec_mode": "cluster",
+	    "instances": 2,
+		"env": {
+			"NODE_ENV": "development",
+		},
+		"env_production" : {
+			"NODE_ENV": "production"
+		}
+	}]
+}
